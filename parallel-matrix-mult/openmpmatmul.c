41bf3aeb56a4c117cc3328ebf9b14c598712bbe0
robotenique <julianogarcia_1997@hotmail.com>
2018-05-18 16:49:57 -0300

    adding some omp

--- parallel-matrix-mult/openmpmatmul.c
+++ parallel-matrix-mult/openmpmatmul.c
@@ -16,10 +16,77 @@
 //     }
 // }
 
-void matmul_omp_2(Matrix A, Matrix B, Matrix C) {
-    //mmo_d(A->n, A->m, B->m, A->matrix, B->matrix, C->matrix);
+void mmb_omp(double** A, int ini_ar, int ini_ac,
+                    double** B, int ini_br, int ini_bc,
+                    double** C, int ini_cr, int ini_cc,
+                    int size_ar, int size_ac, int size_bc, int min_size) {    
+    if (!size_ac || !size_ar || !size_bc)
+        return ;
+    if (size_ar <= min_size && size_ac <= min_size && size_bc <= min_size) {
+        for (int i = 0; i < size_ar; i++) {
+            for (int k = 0; k < size_ac; k++) {
+                for (int j = 0; j < size_bc; j++)
+                    C[ini_cr+i][ini_cc+j] += A[ini_ar+i][ini_ac+k]*B[ini_br+k][ini_bc+j];
+            }
+        }
+        return ;
+    }
+    int new_size_ar = size_ar/2;
+    int new_size_ac = size_ac/2;
+    int new_size_bc = size_bc/2;
+    #pragma omp task untied
+    mmb_omp(A, ini_ar, ini_ac,
+                   B, ini_br, ini_bc,
+                   C, ini_cr, ini_cc,
+                   new_size_ar, new_size_ac, new_size_bc, min_size);
 
+    #pragma omp task untied
+    mmb_omp(A, ini_ar, ini_ac,
+                   B, ini_br, ini_bc + new_size_bc,
+                   C, ini_cr, ini_cc + new_size_bc,
+                   new_size_ar, new_size_ac, size_bc - new_size_bc, min_size);
+    #pragma omp task untied
+    mmb_omp(A, ini_ar + new_size_ar, ini_ac,
+                   B, ini_br, ini_bc,
+                   C, ini_cr + new_size_ar, ini_cc,
+                   size_ar - new_size_ar, new_size_ac, new_size_bc, min_size);
+    #pragma omp task untied
+    mmb_omp(A, ini_ar + new_size_ar, ini_ac,
+                   B, ini_br, ini_bc + new_size_bc,
+                   C, ini_cr + new_size_ar, ini_cc + new_size_bc,
+                   size_ar - new_size_ar, new_size_ac, size_bc - new_size_bc, min_size);
+
+    #pragma omp task untied
+    mmb_omp(A, ini_ar, ini_ac + new_size_ac,
+                   B, ini_br + new_size_ac, ini_bc,
+                   C, ini_cr, ini_cc,
+                   new_size_ar, size_ac - new_size_ac, new_size_bc, min_size);
+    #pragma omp task untied
+    mmb_omp(A, ini_ar, ini_ac + new_size_ac,
+                   B, ini_br + new_size_ac, ini_bc + new_size_bc,
+                   C, ini_cr, ini_cc + new_size_bc,
+                   new_size_ar, size_ac - new_size_ac, size_bc - new_size_bc, min_size);
+    #pragma omp task untied
+    mmb_omp(A, ini_ar + new_size_ar, ini_ac + new_size_ac,
+                   B, ini_br + new_size_ac, ini_bc,
+                   C, ini_cr + new_size_ar, ini_cc,
+                   size_ar - new_size_ar, size_ac - new_size_ac, new_size_bc, min_size);
+    #pragma omp task untied
+    mmb_omp(A, ini_ar + new_size_ar, ini_ac + new_size_ac,
+                   B, ini_br + new_size_ac, ini_bc + new_size_bc,
+                   C, ini_cr + new_size_ar, ini_cc + new_size_bc,
+                   size_ar - new_size_ar, size_ac - new_size_ac, size_bc - new_size_bc, min_size);
+   #pragma omp taskwait
+    return ;
 }
+
+
+void matmul_omp_2(Matrix A, Matrix B, Matrix C) {
+    #pragma omp parallel
+    #pragma omp master
+    mmb_omp(A->matrix, 0, 0, B->matrix, 0, 0, C->matrix, 0, 0, A->n, A->m, B->m, 16);
+}
+
 
 void matmul_omp(MatrixArray A, MatrixArray B, MatrixArray C){
     u_int a_n = A->n;
