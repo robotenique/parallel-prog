c4c0b655d084b8e37745cb94e3c142660cd9bd11
IceMage144 <jgabriel.basi@hotmail.com>
2018-05-17 23:06:17 -0300

    Make matmul_seq2 be the main matmul_seq func

--- parallel-matrix-mult/seqmatmul.c
+++ parallel-matrix-mult/seqmatmul.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "error.h"
 #include "macros.h"
 
 void add(double** C, int ini_cr, int ini_cc,
@@ -48,8 +47,6 @@ void matmul_seq_rec(double** A, int ini_ar, int ini_ac,
     int new_size_ac = size_ac/2;
     int new_size_bc = size_bc/2;
 
-    Matrix T = new_matrix_clean(size_ar, size_bc);
-
     matmul_seq_rec(A, ini_ar, ini_ac,
                    B, ini_br, ini_bc,
                    C, ini_cr, ini_cc,
@@ -69,92 +66,26 @@ void matmul_seq_rec(double** A, int ini_ar, int ini_ac,
 
     matmul_seq_rec(A, ini_ar, ini_ac + new_size_ac,
                    B, ini_br + new_size_ac, ini_bc,
-                   T->matrix, 0, 0,
+                   C, ini_cr, ini_cc,
                    new_size_ar, size_ac - new_size_ac, new_size_bc, min_size);
     matmul_seq_rec(A, ini_ar, ini_ac + new_size_ac,
                    B, ini_br + new_size_ac, ini_bc + new_size_bc,
-                   T->matrix, 0, new_size_bc,
+                   C, ini_cr, ini_cc + new_size_bc,
                    new_size_ar, size_ac - new_size_ac, size_bc - new_size_bc, min_size);
     matmul_seq_rec(A, ini_ar + new_size_ar, ini_ac + new_size_ac,
                    B, ini_br + new_size_ac, ini_bc,
-                   T->matrix, new_size_ar, 0,
+                   C, ini_cr + new_size_ar, ini_cc,
                    size_ar - new_size_ar, size_ac - new_size_ac, new_size_bc, min_size);
     matmul_seq_rec(A, ini_ar + new_size_ar, ini_ac + new_size_ac,
                    B, ini_br + new_size_ac, ini_bc + new_size_bc,
-                   T->matrix, new_size_ar, new_size_bc,
+                   C, ini_cr + new_size_ar, ini_cc + new_size_bc,
                    size_ar - new_size_ar, size_ac - new_size_ac, size_bc - new_size_bc, min_size);
 
-    add(C, ini_cr, ini_cc, T->matrix, 0, 0, size_ar, size_bc);
-    destroy_matrix(T);
     return ;
 }
 
 void matmul_seq(Matrix A, Matrix B, Matrix C, int min_size) {
     matmul_seq_rec(A->matrix, 0, 0, B->matrix, 0, 0, C->matrix, 0, 0, A->n, A->m, B->m, min_size);
-}
-
-void matmul_seq_rec2(double** A, int ini_ar, int ini_ac,
-                    double** B, int ini_br, int ini_bc,
-                    double** C, int ini_cr, int ini_cc,
-                    int size_ar, int size_ac, int size_bc, int min_size) {
-    if (!size_ac || !size_ar || !size_bc)
-        return ;
-    if (size_ar <= min_size && size_ac <= min_size && size_bc <= min_size) {
-        for (int i = 0; i < size_ar; i++) {
-            for (int k = 0; k < size_ac; k++) {
-                for (int j = 0; j < size_bc; j++)
-                    C[ini_cr+i][ini_cc+j] += A[ini_ar+i][ini_ac+k]*B[ini_br+k][ini_bc+j];
-            }
-        }
-        return ;
-    }
-    int new_size_ar = size_ar/2;
-    int new_size_ac = size_ac/2;
-    int new_size_bc = size_bc/2;
-
-    //Matrix T = new_matrix_clean(size_ar, size_bc);
-
-    matmul_seq_rec2(A, ini_ar, ini_ac,
-                   B, ini_br, ini_bc,
-                   C, ini_cr, ini_cc,
-                   new_size_ar, new_size_ac, new_size_bc, min_size);
-    matmul_seq_rec2(A, ini_ar, ini_ac,
-                   B, ini_br, ini_bc + new_size_bc,
-                   C, ini_cr, ini_cc + new_size_bc,
-                   new_size_ar, new_size_ac, size_bc - new_size_bc, min_size);
-    matmul_seq_rec2(A, ini_ar + new_size_ar, ini_ac,
-                   B, ini_br, ini_bc,
-                   C, ini_cr + new_size_ar, ini_cc,
-                   size_ar - new_size_ar, new_size_ac, new_size_bc, min_size);
-    matmul_seq_rec2(A, ini_ar + new_size_ar, ini_ac,
-                   B, ini_br, ini_bc + new_size_bc,
-                   C, ini_cr + new_size_ar, ini_cc + new_size_bc,
-                   size_ar - new_size_ar, new_size_ac, size_bc - new_size_bc, min_size);
-
-    matmul_seq_rec2(A, ini_ar, ini_ac + new_size_ac,
-                   B, ini_br + new_size_ac, ini_bc,
-                   C, ini_cr, ini_cc,
-                   new_size_ar, size_ac - new_size_ac, new_size_bc, min_size);
-    matmul_seq_rec2(A, ini_ar, ini_ac + new_size_ac,
-                   B, ini_br + new_size_ac, ini_bc + new_size_bc,
-                   C, ini_cr, ini_cc + new_size_bc,
-                   new_size_ar, size_ac - new_size_ac, size_bc - new_size_bc, min_size);
-    matmul_seq_rec2(A, ini_ar + new_size_ar, ini_ac + new_size_ac,
-                   B, ini_br + new_size_ac, ini_bc,
-                   C, ini_cr + new_size_ar, ini_cc,
-                   size_ar - new_size_ar, size_ac - new_size_ac, new_size_bc, min_size);
-    matmul_seq_rec2(A, ini_ar + new_size_ar, ini_ac + new_size_ac,
-                   B, ini_br + new_size_ac, ini_bc + new_size_bc,
-                   C, ini_cr + new_size_ar, ini_cc + new_size_bc,
-                   size_ar - new_size_ar, size_ac - new_size_ac, size_bc - new_size_bc, min_size);
-
-    //add(C, ini_cr, ini_cc, T->matrix, 0, 0, size_ar, size_bc);
-    //destroy_matrix(T);
-    return ;
-}
-
-void matmul_seq2(Matrix A, Matrix B, Matrix C, int min_size) {
-    matmul_seq_rec2(A->matrix, 0, 0, B->matrix, 0, 0, C->matrix, 0, 0, A->n, A->m, B->m, min_size);
 }
 
 void matmul_trashy(Matrix A, Matrix B, Matrix C){
@@ -190,3 +121,4 @@ void matcoisa(MatrixArray A, MatrixArray B, MatrixArray C){
             }
          }
     }
+}
--- parallel-matrix-mult/seqmatmul.h
+++ parallel-matrix-mult/seqmatmul.h
@@ -11,10 +11,13 @@
 #ifndef __SEQMATMUL_H__
 #define __SEQMATMUL_H__
 
-#define MIN_SIZE 512
+#define MIN_SIZE 256
+
+void add(double** C, int ini_cr, int ini_cc,
+         double** T, int ini_tr, int ini_tc,
+         int size_r, int size_c);
 
 void matmul_seq(Matrix A, Matrix B, Matrix C, int min_size);
-void matmul_seq2(Matrix A, Matrix B, Matrix C, int min_size);
 
 //TODO: REMOVE THIS
 void matmul_trashy(Matrix A, Matrix B, Matrix C);
