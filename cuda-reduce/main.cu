f2870913f8dc3cf36a1dfdcd7983eb8fb3fbffce
IceMage144 <jgabriel.basi@hotmail.com>
2018-06-14 18:28:30 -0300

    Fix some errors

--- cuda-reduce/main.cu
+++ cuda-reduce/main.cu
@@ -10,15 +10,19 @@ __global__ void reduce_min( int32_t *mats, int32_t N ) {
     int tid = 9*(threadIdx.x + blockIdx.x * blockDim.x);
     int cid = 9*threadIdx.x;
 
-    for (int32_t i = 0; i < 9; i++)
-        cache[threadIdx.x + i] = mats[tid + i];
+
+    for (int32_t i = 0; i < 9; i++) {
+        cache[cid + i] = mats[tid + i];
+    }
 
     __syncthreads();
 
     for (int32_t i = blockDim.x/2; i != 0; i >>= 1) {
         if (threadIdx.x < i) {
-            for (int32_t j = 0; j < 9; j++)
-                cache[threadIdx.x] = cache[threadIdx.x + j] + ((cache[threadIdx.x + i + j]-cache[threadIdx.x + j])&((cache[threadIdx.x + i + j]-cache[threadIdx.x + j]) >> 31));
+            for (int32_t j = 0; j < 9; j++) {
+                int32_t d = cache[cid + 9*i + j] - cache[cid + j];
+                cache[cid + j] = cache[cid + j] + (d&(d >> 31));
+            }
         }
         __syncthreads();
     }
@@ -42,18 +46,18 @@ int main(int argc, char const *argv[]) {
     cout << "=======REDUCED (SEQ)=======" << '\n';
     print_matrices(mat_reduced, 1);
 
-    int32_t  *cuda_result = new int32_t[9*num_m];
+    int32_t  *cuda_result = new int32_t[9];
     ecudaMalloc((void **)&d_list_m, 9*num_m*sizeof(int32_t));
     cout << "---- COPIA 1 ----\n";
     int32_t *coisa;
     new_matrix_from_file(argv[1], &coisa);
     ecudaMemcpy(d_list_m, coisa, 9*num_m*sizeof(int32_t), cudaMemcpyHostToDevice);
     cout << "---- COPIA 1 {end} ----\n";
-    reduce_min<<<num_m/NUM_THREADS, NUM_THREADS, 9*NUM_THREADS>>>(d_list_m, num_m);
+    reduce_min<<<num_m/NUM_THREADS, NUM_THREADS, 9*NUM_THREADS*sizeof(int32_t)>>>(d_list_m, num_m);
     cout << "---- COPIA 2 ----\n";
-    ecudaMemcpy(cuda_result, d_list_m, 9*num_m*sizeof(int32_t), cudaMemcpyDeviceToHost);
+    ecudaMemcpy(cuda_result, d_list_m, 9*sizeof(int32_t), cudaMemcpyDeviceToHost);
     cout << "---- COPIA 2 {end} ----\n";
-    print_matrices(cuda_result, num_m);
+    print_matrices(cuda_result, 1);
 
     ecudaFree(d_list_m);
     delete[] list_m;
